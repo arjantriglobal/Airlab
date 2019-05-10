68aee579063b1840fd7ea305ac56a701dffc0ee2
Johnny Bakker <johnnybakker1997@gmail.com>
2019-05-10 13:34:53 +0200

    Added a default user account to the user seeder

--- application/database/seeds/UsersTableSeeder.php
+++ application/database/seeds/UsersTableSeeder.php
@@ -14,6 +14,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
+        //Add default admin user if this not already exists.
         $users = User::where("email", "=", "admin@mail.nl")->get();
         if(count($users) == 0){
             $admin = new User();
@@ -21,6 +22,23 @@ class UsersTableSeeder extends Seeder
             $admin->email = "admin@mail.nl";
             $admin->password = Hash::make("123admin");
             $admin->role = 2;
+            $admin->created_at = Carbon::now()->format('Y-m-d H:i:s');
+
+            $organizations = Organization::where("name", "=", "Da Vinci College")->get();
+            if(count($organizations) > 0){
+                $admin->organization_id = $organizations[0]->id;
+                $admin->save();
+            }
+        }
+
+        //Add default user if this not already exists.
+        $users = User::where("email", "=", "user@mail.nl")->get();
+        if(count($users) == 0){
+            $admin = new User();
+            $admin->name = "User";
+            $admin->email = "user@mail.nl";
+            $admin->password = Hash::make("123user");
+            $admin->role = 1;
             $admin->created_at = Carbon::now()->format('Y-m-d H:i:s');
 
             $organizations = Organization::where("name", "=", "Da Vinci College")->get();
